01a66e3b7edfee00d15819dc1d506cc65f16eb8d
Jiaming740 <57663050+Jiaming740@users.noreply.github.com>
2024-10-10 18:38:01 +0800

    Add files via upload

--- data/data_proc.py
+++ data/data_proc.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import numpy as np
 import pandas as pd
 import json
 from collections import Counter
-import re
 
 def main():
     path = './uspto-release'
